2f9e159757792d7495e8cd430d01f25961dfeb44
Tetiana <Tetiana@mbp-tetiana.speedport.ip>
2024-02-05 17:22:57 +0100

    add colors to logger

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const logger = require('./utils/logger')('Main');
+require('dotenv').config();
+const config = require('config');
+
+const logger = require('./utils/logger')('Main', config.logger);
 
 logger.info('Some information...');
 logger.error('Some error...');
--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,29 @@
-const logger = (moduleName) => {
-  return {
-    info: (...args) => console.log('[INFO] ${moduleName}: ', ...args),
-    warn: (...args) => console.log('[WARN] ${moduleName}: ', ...args),
-    error: (...args) => console.log('[ERROR] ${moduleName}: ', ...args),
+const colors = require('colors/safe');
+
+const logger = (moduleName, options) => {
+  const { colorsEnabled, logLevel } = options;
+
+  if (!colorsEnabled) {
+    colors.disable();
   }
-}
+
+  const logLevelsPriority = {
+    info: 0,
+    warn: 1,
+    error: 2,
+  };
+
+  return {
+    info: (...args) => {
+      if (logLevelsPriority[logLevel] > logLevelsPriority.info) return;
+      console.log(colors.cyan(`[INFO] ${moduleName}:`), ...args);
+    },
+    warn: (...args) => {
+      if (logLevelsPriority[logLevel] > logLevelsPriority.warn) return;
+      console.log(colors.yellow(`[WARN] ${moduleName}:`), ...args);
+    },
+    error: (...args) => console.error(colors.red(`[ERROR] ${moduleName}:`), ...args),
+  };
+};
 
 module.exports = logger;
